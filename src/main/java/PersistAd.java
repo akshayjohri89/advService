d57ed25479f3fd4a7857e174807125336be01167
Akshay Johri <akjohri@microsoft.com>
2017-08-01 11:56:25 +0530

    last test

--- src/main/java/PersistAd.java
+++ src/main/java/PersistAd.java
@@ -36,7 +36,7 @@ public class PersistAd {
         String method = "publish";
         String id = "1";
         List<Object> params = new ArrayList<Object>();
-        params.add("adstream2");
+        params.add("adstream1");
         Long uuid = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
         params.add(uuid.toString());
         params.add(hex);
