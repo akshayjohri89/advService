c2309e7902040540faf1d5754a9cd5eed0bf0e13
Akshay Johri <akjohri@microsoft.com>
2018-07-26 01:41:01 +0530

    AllAds

--- src/main/java/RetrieveAds.java
+++ src/main/java/RetrieveAds.java
@@ -80,6 +80,7 @@ public class RetrieveAds {
                 for (int index=0;index<=tokenList.length();index++) {
                     JSONObject ob = tokenList.getJSONObject(index);
                     String data = ob.getString("data");
+                    System.out.println("AllAds:Data:"+data);
                     StringBuilder output = new StringBuilder();
                     for (int i = 0; i < data.length(); i+=2) {
                         String str = data.substring(i, i+2);
