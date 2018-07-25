bef54c7a9eaeaed8b73f55f05d6d7faeff1a0ff1
Akshay Johri <akjohri@microsoft.com>
2018-07-26 01:57:42 +0530

    Blank Ad fix

--- src/main/java/RetrieveAds.java
+++ src/main/java/RetrieveAds.java
@@ -80,15 +80,17 @@ public class RetrieveAds {
                 for (int index=0;index<=tokenList.length();index++) {
                     JSONObject ob = tokenList.getJSONObject(index);
                     String data = ob.getString("data");
-                    StringBuilder output = new StringBuilder();
-                    for (int i = 0; i < data.length(); i+=2) {
-                        String str = data.substring(i, i+2);
-                        output.append((char)Integer.parseInt(str, 16));
+                    if (data!=null) {
+                        StringBuilder output = new StringBuilder();
+                        for (int i = 0; i < data.length(); i+=2) {
+                            String str = data.substring(i, i+2);
+                            output.append((char)Integer.parseInt(str, 16));
+                        }
+                        System.out.println("AllAds:Data:"+output.toString());
+                        JSONObject returnJson = new JSONObject(output.toString());
+                        returnJson.put("key",ob.getString("key"));
+                        toReturn.add(returnJson.toString());
                     }
-                    System.out.println("AllAds:Data:"+output.toString());
-                    JSONObject returnJson = new JSONObject(output.toString());
-                    returnJson.put("key",ob.getString("key"));
-                    toReturn.add(returnJson.toString());
                 }
 //                    toReturn = oj.getString("data");
 //                    return output.toString();
